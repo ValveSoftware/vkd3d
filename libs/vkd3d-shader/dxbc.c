03422e96819d0015c33904bce4a26dfb28d44331
Conor McCarthy <cmccarthy@codeweavers.com>
2021-07-01 14:24:52 +1000

    vkd3d-shader: Call shader_sm4_set_descriptor_register_range() for structured resources.
    Signed-off-by: Conor McCarthy <cmccarthy@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- libs/vkd3d-shader/dxbc.c
+++ libs/vkd3d-shader/dxbc.c
@@ -976,8 +976,7 @@ static void shader_sm5_read_dcl_resource_structured(struct vkd3d_shader_instruct
     const DWORD *end = &tokens[token_count];
 
     shader_sm4_read_dst_param(priv, &tokens, end, VKD3D_DATA_RESOURCE, &resource->resource.reg);
-    resource->resource.range.first = shader_sm4_map_resource_idx(&resource->resource.reg.reg, priv);
-    resource->resource.range.last = resource->resource.range.first;
+    shader_sm4_set_descriptor_register_range(priv, &resource->resource.reg.reg, &resource->resource.range);
     resource->byte_stride = *tokens++;
     if (resource->byte_stride % 4)
         FIXME("Byte stride %u is not multiple of 4.\n", resource->byte_stride);
