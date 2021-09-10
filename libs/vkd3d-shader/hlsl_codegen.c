15d3a560925ff217fc4c53ac0b41cbcd8ad6bc56
Zebediah Figura <zfigura@codeweavers.com>
2021-09-10 17:39:44 -0500

    vkd3d-shader/hlsl: Fold constant swizzles.

--- libs/vkd3d-shader/hlsl_codegen.c
+++ libs/vkd3d-shader/hlsl_codegen.c
@@ -2356,7 +2356,8 @@ int hlsl_emit_bytecode(struct hlsl_ctx *ctx, struct hlsl_ir_function_decl *entry
     transform_ir(ctx, lower_casts_to_bool, body, NULL);
     do
     {
-        progress = transform_ir(ctx, hlsl_fold_constants, body, NULL);
+        progress = transform_ir(ctx, hlsl_fold_constant_exprs, body, NULL);
+        progress |= transform_ir(ctx, hlsl_fold_constant_swizzles, body, NULL);
         progress |= copy_propagation_execute(ctx, body);
         progress |= transform_ir(ctx, split_swizzled_load, body, NULL);
         progress |= transform_ir(ctx, remove_trivial_swizzles, body, NULL);
