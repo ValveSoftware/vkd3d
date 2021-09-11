ac2918d833130fe5512aac3ac7d5a3c6f8856af0
Zebediah Figura <zfigura@codeweavers.com>
2021-09-11 18:21:54 -0500

    [FIXME] vkd3d-shader/hlsl: Watch for an overloaded entry point.
    FIXME: this is cheap; I already wrote the tests...

--- libs/vkd3d-shader/hlsl.c
+++ libs/vkd3d-shader/hlsl.c
@@ -2129,8 +2129,12 @@ int hlsl_compile_shader(const struct vkd3d_shader_code *hlsl, const struct vkd3d
         {
             if (!decl->has_body)
                 continue;
+            if (entry_func)
+            {
+                hlsl_fixme(&ctx, decl->loc, "Overloaded entry point.");
+                break;
+            }
             entry_func = decl;
-            break;
         }
     }
 
