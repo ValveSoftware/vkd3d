60c80b40cc10146b7c870628ca72cfe256fbd71e
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-09-21 13:33:30 +0200

    vkd3d-shader/hlsl: Write SM4 int bitwise XOR.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1683,6 +1683,10 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_OR, &expr->node, arg1, arg2);
                     break;
 
+                case HLSL_OP2_BIT_XOR:
+                    write_sm4_binary_op(buffer, VKD3D_SM4_OP_XOR, &expr->node, arg1, arg2);
+                    break;
+
                 case HLSL_OP2_MAX:
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_IMAX, &expr->node, arg1, arg2);
                     break;
