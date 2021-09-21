c6caef97b0003bb00873bc31e3f2170992816a53
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-09-21 13:44:01 +0200

    vkd3d-shader/hlsl: Write SM4 int right shift.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1695,6 +1695,10 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_ISHL, &expr->node, arg1, arg2);
                     break;
 
+                case HLSL_OP2_RSHIFT:
+                    write_sm4_binary_op(buffer, VKD3D_SM4_OP_ISHR, &expr->node, arg1, arg2);
+                    break;
+
                 case HLSL_OP2_MAX:
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_IMAX, &expr->node, arg1, arg2);
                     break;
