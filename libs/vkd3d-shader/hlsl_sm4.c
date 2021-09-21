492b7bbea95eb508bd9a259df015d2de79999bd8
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-09-21 13:32:53 +0200

    vkd3d-shader/hlsl: Write SM4 int bitwise AND.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1675,6 +1675,10 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
                     write_sm4_unary_op(buffer, VKD3D_SM4_OP_INEG, &expr->node, arg1, 0);
                     break;
 
+                case HLSL_OP2_BIT_AND:
+                    write_sm4_binary_op(buffer, VKD3D_SM4_OP_AND, &expr->node, arg1, arg2);
+                    break;
+
                 case HLSL_OP2_MAX:
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_IMAX, &expr->node, arg1, arg2);
                     break;
