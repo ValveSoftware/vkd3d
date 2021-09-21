706e10951f4aebd4b0826d2d7d1d126f08b25269
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-09-21 13:42:36 +0200

    vkd3d-shader/hlsl: Write SM4 uint left shift.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1778,6 +1778,10 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
                     write_sm4_unary_op(buffer, VKD3D_SM4_OP_NOT, &expr->node, arg1, 0);
                     break;
 
+                case HLSL_OP2_LSHIFT:
+                    write_sm4_binary_op(buffer, VKD3D_SM4_OP_ISHL, &expr->node, arg1, arg2);
+                    break;
+
                 default:
                     hlsl_fixme(ctx, expr->node.loc, "SM4 uint \"%s\" expression.\n", debug_hlsl_expr_op(expr->op));
                     break;
