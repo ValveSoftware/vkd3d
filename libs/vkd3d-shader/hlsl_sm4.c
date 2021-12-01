790f330836874a6ab9c469905346fa7d38e7badf
Giovanni Mascellani <gmascellani@codeweavers.com>
2021-12-01 17:15:03 +0100

    vkd3d-shader/hlsl: Write SM4 integer minimum.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1424,6 +1424,10 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
                     write_sm4_binary_op(buffer, VKD3D_SM4_OP_IMAX, &expr->node, arg1, arg2);
                     break;
 
+                case HLSL_OP2_MIN:
+                    write_sm4_binary_op(buffer, VKD3D_SM4_OP_IMIN, &expr->node, arg1, arg2);
+                    break;
+
                 default:
                     hlsl_fixme(ctx, &expr->node.loc, "SM4 int \"%s\" expression.", debug_hlsl_expr_op(expr->op));
                     break;
