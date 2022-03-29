75ab9d31efcd06c8f50c4bce2c01331e630d3a49
Zebediah Figura <zfigura@codeweavers.com>
2022-03-29 18:49:10 -0500

    vkd3d-shader/hlsl: Store the matrix majority as a type modifiers bitmask.

--- libs/vkd3d-shader/hlsl.h
+++ libs/vkd3d-shader/hlsl.h
@@ -116,12 +116,6 @@ enum hlsl_sampler_dim
    HLSL_SAMPLER_DIM_MAX = HLSL_SAMPLER_DIM_CUBEARRAY,
 };
 
-enum hlsl_matrix_majority
-{
-    HLSL_COLUMN_MAJOR,
-    HLSL_ROW_MAJOR
-};
-
 enum hlsl_regset
 {
     HLSL_REGSET_SAMPLERS,
@@ -735,7 +729,7 @@ struct hlsl_ctx
     const struct hlsl_ir_function_decl *cur_function;
 
     /* Default matrix majority for matrix types. Can be set by a pragma within the HLSL source. */
-    enum hlsl_matrix_majority matrix_majority;
+    unsigned int matrix_majority;
 
     /* Basic data types stored for convenience. */
     struct
