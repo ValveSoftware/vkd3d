448f856c0982b00a4c49a87ca952f9f4427f2c43
Giovanni Mascellani <gmascellani@codeweavers.com>
2022-07-01 11:23:33 +0200

    vkd3d-shader/hlsl: Write SM4 logic or instructions.
    Signed-off-by: Giovanni Mascellani <gmascellani@codeweavers.com>
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- libs/vkd3d-shader/hlsl_sm4.c
+++ libs/vkd3d-shader/hlsl_sm4.c
@@ -1851,6 +1851,11 @@ static void write_sm4_expr(struct hlsl_ctx *ctx,
             write_sm4_binary_op(buffer, VKD3D_SM4_OP_AND, &expr->node, arg1, arg2);
             break;
 
+        case HLSL_OP2_LOGIC_OR:
+            assert(dst_type->base_type == HLSL_TYPE_BOOL);
+            write_sm4_binary_op(buffer, VKD3D_SM4_OP_OR, &expr->node, arg1, arg2);
+            break;
+
         case HLSL_OP2_LSHIFT:
             assert(type_is_integer(dst_type));
             assert(dst_type->base_type != HLSL_TYPE_BOOL);
