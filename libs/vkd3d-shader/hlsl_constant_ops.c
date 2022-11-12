c805eb1191eb0a9b576cf2b83bbb32cc81f24e41
Zebediah Figura <zfigura@codeweavers.com>
2022-11-11 18:55:55 -0600

    vkd3d-shader/hlsl: Pass hlsl_constant_value and hlsl_type pointers to fold_min().

--- libs/vkd3d-shader/hlsl_constant_ops.c
+++ libs/vkd3d-shader/hlsl_constant_ops.c
@@ -434,29 +434,29 @@ static bool fold_max(struct hlsl_ctx *ctx, struct hlsl_constant_value *dst, cons
     return true;
 }
 
-static bool fold_min(struct hlsl_ctx *ctx, struct hlsl_ir_constant *dst,
-        struct hlsl_ir_constant *src1, struct hlsl_ir_constant *src2)
+static bool fold_min(struct hlsl_ctx *ctx, struct hlsl_constant_value *dst, const struct hlsl_type *dst_type,
+        const struct hlsl_ir_constant *src1, const struct hlsl_ir_constant *src2)
 {
-    enum hlsl_base_type type = dst->node.data_type->base_type;
+    enum hlsl_base_type type = dst_type->base_type;
     unsigned int k;
 
     assert(type == src1->node.data_type->base_type);
     assert(type == src2->node.data_type->base_type);
 
-    for (k = 0; k < dst->node.data_type->dimx; ++k)
+    for (k = 0; k < dst_type->dimx; ++k)
     {
         switch (type)
         {
             case HLSL_TYPE_INT:
-                dst->value.u[k].i = min(src1->value.u[k].i, src2->value.u[k].i);
+                dst->u[k].i = min(src1->value.u[k].i, src2->value.u[k].i);
                 break;
 
             case HLSL_TYPE_UINT:
-                dst->value.u[k].u = min(src1->value.u[k].u, src2->value.u[k].u);
+                dst->u[k].u = min(src1->value.u[k].u, src2->value.u[k].u);
                 break;
 
             default:
-                FIXME("Fold min for type %s.\n", debug_hlsl_type(ctx, dst->node.data_type));
+                FIXME("Fold min for type %s.\n", debug_hlsl_type(ctx, dst_type));
                 return false;
         }
     }
@@ -612,7 +612,7 @@ bool hlsl_fold_constant_exprs(struct hlsl_ctx *ctx, struct hlsl_ir_node *instr, 
             break;
 
         case HLSL_OP2_MIN:
-            success = fold_min(ctx, res, arg1, arg2);
+            success = fold_min(ctx, &res->value, instr->data_type, arg1, arg2);
             break;
 
         case HLSL_OP2_BIT_XOR:
