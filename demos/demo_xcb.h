57d92a15cf93d9448f2deb91a8614f9c4c085216
Henri Verbeet <hverbeet@codeweavers.com>
2023-03-14 17:29:04 +0100

    Release 1.7.

--- demos/demo_xcb.h
+++ demos/demo_xcb.h
@@ -19,7 +19,7 @@
 
 #define VK_NO_PROTOTYPES
 #define VK_USE_PLATFORM_XCB_KHR
-#define VKD3D_UTILS_API_VERSION VKD3D_API_VERSION_1_6
+#define VKD3D_UTILS_API_VERSION VKD3D_API_VERSION_1_7
 #include "config.h"
 #include <vkd3d.h>
 #include <vkd3d_utils.h>
