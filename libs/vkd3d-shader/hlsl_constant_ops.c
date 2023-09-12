c16c5caad8e6c73fb5b8e5ac15e5ccd9b647cdde
Nikolay Sivov <nsivov@codeweavers.com>
2023-09-12 18:31:03 +0200

    vkd3d-shader/hlsl: Add constant folding for 'fract'.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- libs/vkd3d-shader/hlsl_constant_ops.c
+++ libs/vkd3d-shader/hlsl_constant_ops.c
@@ -202,6 +202,33 @@ static bool fold_cast(struct hlsl_ctx *ctx, struct hlsl_constant_value *dst,
     return true;
 }
 
+static bool fold_fract(struct hlsl_ctx *ctx, struct hlsl_constant_value *dst,
+        const struct hlsl_type *dst_type, const struct hlsl_ir_constant *src)
+{
+    enum hlsl_base_type type = dst_type->base_type;
+    unsigned int k;
+    float i;
+
+    assert(type == src->node.data_type->base_type);
+
+    for (k = 0; k < dst_type->dimx; ++k)
+    {
+        switch (type)
+        {
+            case HLSL_TYPE_FLOAT:
+            case HLSL_TYPE_HALF:
+                dst->u[k].f = modff(src->value.u[k].f, &i);
+                break;
+
+            default:
+                FIXME("Fold 'fract' for type %s.\n", debug_hlsl_type(ctx, dst_type));
+                return false;
+        }
+    }
+
+    return true;
+}
+
 static bool fold_log2(struct hlsl_ctx *ctx, struct hlsl_constant_value *dst, const struct hlsl_type *dst_type,
         const struct hlsl_ir_constant *src, const struct vkd3d_shader_location *loc)
 {
@@ -994,6 +1021,10 @@ bool hlsl_fold_constant_exprs(struct hlsl_ctx *ctx, struct hlsl_ir_node *instr, 
 
         case HLSL_OP1_CAST:
             success = fold_cast(ctx, &res, instr->data_type, arg1);
+            break;
+
+        case HLSL_OP1_FRACT:
+            success = fold_fract(ctx, &res, instr->data_type, arg1);
             break;
 
         case HLSL_OP1_LOG2:
