4c7d956bdb4e5d5b12802ad0b0f85f85cbd83fb3
Conor McCarthy <cmccarthy@codeweavers.com>
2024-02-14 00:13:57 +1000

    vkd3d-shader/dxil: Check for src param allocation failure in sm6_parser_emit_load().

--- libs/vkd3d-shader/dxil.c
+++ libs/vkd3d-shader/dxil.c
@@ -5114,7 +5114,8 @@ static void sm6_parser_emit_load(struct sm6_parser *sm6, const struct dxil_recor
 
     vsir_instruction_init(ins, &sm6->p.location, VKD3DSIH_MOV);
 
-    src_param = instruction_src_params_alloc(ins, 1, sm6);
+    if (!(src_param = instruction_src_params_alloc(ins, 1, sm6)))
+        return;
     src_param_init_from_value(&src_param[0], ptr);
     src_param->reg.alignment = alignment;
 
