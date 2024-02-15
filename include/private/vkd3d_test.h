4735ff48d17258fa996a9df954dde2893482aefb
Henri Verbeet <hverbeet@codeweavers.com>
2024-02-15 14:51:42 +0100

    tests: Disable buffering of stdout.
    Both to avoid it getting lost in case of crashes, and to properly interleave
    test output and debug output in the log files.

--- include/private/vkd3d_test.h
+++ include/private/vkd3d_test.h
@@ -277,6 +277,7 @@ int main(int argc, char **argv)
     char *test_platform = getenv("VKD3D_TEST_PLATFORM");
     const char *bug = getenv("VKD3D_TEST_BUG");
 
+    setvbuf(stdout, NULL, _IONBF, 0);
     memset(&vkd3d_test_state, 0, sizeof(vkd3d_test_state));
     vkd3d_test_state.debug_level = debug_level ? atoi(debug_level) : 0;
     vkd3d_test_state.bug_enabled = bug ? atoi(bug) : true;
