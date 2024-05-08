7c7503713a0e898c77ff217e26d3de70a3f69f4d
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-05-08 11:25:22 +0200

    vkd3d: Handle D3D12_FEATURE_D3D12_OPTIONS17 in CheckFeatureSupport().

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -3972,6 +3972,23 @@ static HRESULT STDMETHODCALLTYPE d3d12_device_CheckFeatureSupport(ID3D12Device9 
             return S_OK;
         }
 
+        case D3D12_FEATURE_D3D12_OPTIONS17:
+        {
+            D3D12_FEATURE_DATA_D3D12_OPTIONS17 *data = feature_data;
+
+            if (feature_data_size != sizeof(*data))
+            {
+                WARN("Invalid size %u.\n", feature_data_size);
+            }
+
+            data->NonNormalizedCoordinateSamplersSupported = FALSE;
+            data->ManualWriteTrackingResourceSupported = FALSE;
+
+            TRACE("Non-normalized coordinate samplers %#x.\n", data->NonNormalizedCoordinateSamplersSupported);
+            TRACE("Manual write tracking resource %#x.\n", data->ManualWriteTrackingResourceSupported);
+            return S_OK;
+        }
+
         default:
             FIXME("Unhandled feature %#x.\n", feature);
             return E_NOTIMPL;
