ValveSoftware/vkd3d

commit cf7c05f4315dd21a96f60653497c1228a38f61af

vkd3d-shader: Remove a token for 'precise' modifier.
Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

libs/vkd3d-shader/hlsl.y
@@ -5363,7 +5363,6 @@ static void validate_uav_type(struct hlsl_ctx *ctx, enum hlsl_sampler_dim dim,
 %token KW_PACKOFFSET
 %token KW_PASS
 %token KW_PIXELSHADER
-%token KW_PRECISE
 %token KW_RASTERIZERORDEREDBUFFER
 %token KW_RASTERIZERORDEREDSTRUCTUREDBUFFER
 %token KW_RASTERIZERORDEREDTEXTURE1D
@@ -6823,10 +6822,6 @@ var_modifiers:
         {
             $$ = add_modifiers(ctx, $2, HLSL_STORAGE_NOPERSPECTIVE, &@1);
         }
-    | KW_PRECISE var_modifiers
-        {
-            $$ = add_modifiers(ctx, $2, HLSL_MODIFIER_PRECISE, &@1);
-        }
     | KW_SHARED var_modifiers
         {
             $$ = add_modifiers(ctx, $2, HLSL_STORAGE_SHARED, &@1);
@@ -6875,7 +6870,14 @@ var_modifiers:
         {
             $$ = add_modifiers(ctx, $2, HLSL_MODIFIER_INLINE, &@1);
         }
-
+    | var_identifier var_modifiers
+        {
+            if (!strcmp($1, "precise"))
+                $$ = add_modifiers(ctx, $2, HLSL_MODIFIER_PRECISE, &@1);
+            else
+                hlsl_error(ctx, &@1, VKD3D_SHADER_ERROR_HLSL_UNKNOWN_MODIFIER,
+                        "Unknown modifier %s.", debugstr_a($1));
+        }
 
 complex_initializer:
       initializer_expr

Makefile.am
@@ -155,6 +155,7 @@ vkd3d_shader_tests = \
 	tests/hlsl/object-parameters.shader_test \
 	tests/hlsl/object-references.shader_test \
 	tests/hlsl/pow.shader_test \
+	tests/hlsl/precise-modifier.shader_test \
 	tests/hlsl/rasteriser-ordered-views.shader_test \
 	tests/hlsl/reflect.shader_test \
 	tests/hlsl/register-reservations-numeric.shader_test \

libs/vkd3d-shader/hlsl.l
@@ -109,7 +109,6 @@ packoffset              {return KW_PACKOFFSET;          }
 pass                    {return KW_PASS;                }
 PixelShader             {return KW_PIXELSHADER;         }
 pixelshader             {return KW_PIXELSHADER;         }
-precise                 {return KW_PRECISE;             }
 RasterizerOrderedBuffer           {return KW_RASTERIZERORDEREDBUFFER;            }
 RasterizerOrderedStructuredBuffer {return KW_RASTERIZERORDEREDSTRUCTUREDBUFFER;  }
 RasterizerOrderedTexture1D        {return KW_RASTERIZERORDEREDTEXTURE1D;         }

libs/vkd3d-shader/vkd3d_shader_private.h
@@ -149,6 +149,7 @@ enum vkd3d_shader_error
     VKD3D_SHADER_ERROR_HLSL_NON_FINITE_RESULT           = 5027,
     VKD3D_SHADER_ERROR_HLSL_DUPLICATE_SWITCH_CASE       = 5028,
     VKD3D_SHADER_ERROR_HLSL_MISSING_TECHNIQUE           = 5029,
+    VKD3D_SHADER_ERROR_HLSL_UNKNOWN_MODIFIER            = 5030,
 
     VKD3D_SHADER_WARNING_HLSL_IMPLICIT_TRUNCATION       = 5300,
     VKD3D_SHADER_WARNING_HLSL_DIVISION_BY_ZERO          = 5301,

tests/hlsl/precise-modifier.shader_test
@@ -1,0 +1,41 @@
+[pixel shader]
+// 'precise' is not a keyword
+float4 precise;
+
+float4 main() : sv_target
+{
+    return precise;
+}
+
+[test]
+uniform 0 float4 0.1 0.2 0.3 0.4
+draw quad
+probe all rgba (0.1, 0.2, 0.3, 0.4)
+
+[pixel shader fail(sm>=6)]
+precise float4 var;
+typedef float4 precise;
+
+precise main() : sv_target
+{
+    return var;
+}
+
+[test]
+uniform 0 float4 0.1 0.2 0.3 0.4
+draw quad
+probe all rgba (0.1, 0.2, 0.3, 0.4)
+
+[pixel shader]
+float4 precise;
+precise float4 var;
+
+float4 main() : sv_target
+{
+    return precise;
+}
+
+[test]
+uniform 0 float4 0.1 0.2 0.3 0.4
+draw quad
+probe all rgba (0.1, 0.2, 0.3, 0.4)

commit 2f88442d7d51aed1ff597ec3df31ce12ed3b54a6

vkd3d-shader/hlsl: Always initialize $$ when parsing modifiers from an arbitrary string.
This avoids spurious errors like "row_major and column_major are mutually
exclusive" when an invalid modifier is used.

Fixes: cf7c05f4315dd21a96f60653497c1228a38f61af

libs/vkd3d-shader/hlsl.y
@@ -7296,6 +7296,8 @@ var_modifiers:
         }
     | var_identifier var_modifiers
         {
+            $$ = $2;
+
             if (!strcmp($1, "precise"))
                 $$ = add_modifiers(ctx, $2, HLSL_MODIFIER_PRECISE, &@1);
             else if (!strcmp($1, "single"))